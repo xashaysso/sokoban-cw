4088642b0c95f0713ff345bc198bef31c9277366
xashaysso <godzillayttop1234@gmail.com>
2026-08-18 17:29:36 +0300

    refactor: small cosmetic changes to leaderboards

--- graphics/WinRenderer.cpp
+++ graphics/WinRenderer.cpp
@@ -13,31 +13,33 @@ WinRenderer::WinRenderer()
         std::cerr << "Failed to load font." << std::endl;
     }
 
-    titleText.setCharacterSize(38);
+    titleText.setCharacterSize(44);
     titleText.setFillColor(sf::Color::Yellow);
 
-    currentStatsText.setCharacterSize(22);
+    currentStatsText.setCharacterSize(26);
     currentStatsText.setFillColor(sf::Color(100, 255, 100));
 
-    loadingText.setCharacterSize(24);
+    loadingText.setCharacterSize(28);
     loadingText.setFillColor(sf::Color::Cyan);
     loadingText.setString("Loading leaderboard...");
 
-    headerText.setCharacterSize(20);
+    headerText.setCharacterSize(24);
     headerText.setFillColor(sf::Color(255, 215, 0));
 
-    rowText.setCharacterSize(20);
+    rowText.setCharacterSize(24);
     rowText.setFillColor(sf::Color::White);
 
-    hintText.setCharacterSize(20);
+    hintText.setCharacterSize(22);
     hintText.setFillColor(sf::Color(200, 200, 200));
 
-    overlay.setFillColor(sf::Color(0, 0, 0, 180));
+    overlay.setFillColor(sf::Color(0, 0, 0, 190));
 
-    tableBox.setSize({620.f, 260.f});
-    tableBox.setFillColor(sf::Color(30, 30, 40, 220));
-    tableBox.setOutlineColor(sf::Color(70, 70, 90));
-    tableBox.setOutlineThickness(2.f);
+    tableBox.setSize({760.f, 420.f});
+    tableBox.setFillColor(sf::Color(25, 25, 35, 235));
+    tableBox.setOutlineColor(sf::Color(70, 70, 100));
+    tableBox.setOutlineThickness(3.f);
+    tableBox.setOrigin({380.f, 210.f});
+    tableBox.setPosition({512.f, 400.f});
 }
 
 void WinRenderer::render(sf::RenderWindow &window, int steps, float time, bool isLastLevel,
@@ -54,14 +56,7 @@ void WinRenderer::render(sf::RenderWindow &window, int steps, float time, bool i
 
     if (isLastLevel) {
         titleText.setString("ALL LEVELS COMPLETED!");
-        hintText.setString("Press Enter to return to Menu...");
-    } else if (hasError) {
-        rowText.setFillColor(sf::Color(255, 100, 100));
-        rowText.setString("Could not connect to server");
-        auto rBounds = rowText.getLocalBounds();
-        rowText.setOrigin({rBounds.position.x + rBounds.size.x / 2.f, rBounds.position.y + rBounds.size.y / 2.f});
-        rowText.setPosition({512.f, 290.f});
-        window.draw(rowText);
+        hintText.setString("Press ENTER to return to Menu");
     } else {
         titleText.setString("LEVEL COMPLETED!");
         hintText.setString("Press ENTER for Next Level / ESC for Menu");
@@ -69,29 +64,35 @@ void WinRenderer::render(sf::RenderWindow &window, int steps, float time, bool i
 
     auto tBounds = titleText.getLocalBounds();
     titleText.setOrigin({tBounds.position.x + tBounds.size.x / 2.f, tBounds.position.y + tBounds.size.y / 2.f});
-    titleText.setPosition({512.f, 80.f});
+    titleText.setPosition({512.f, 60.f});
     window.draw(titleText);
 
     currentStatsText.setString("Your Run: " + std::to_string(steps) + " steps  |  Time: " + timeStr);
     auto cBounds = currentStatsText.getLocalBounds();
     currentStatsText.setOrigin({cBounds.position.x + cBounds.size.x / 2.f, cBounds.position.y + cBounds.size.y / 2.f});
-    currentStatsText.setPosition({512.f, 130.f});
+    currentStatsText.setPosition({512.f, 120.f});
     window.draw(currentStatsText);
 
-    tableBox.setPosition({202.f, 170.f});
     window.draw(tableBox);
 
     if (isLoading) {
         auto lBounds = loadingText.getLocalBounds();
         loadingText.setOrigin({lBounds.position.x + lBounds.size.x / 2.f, lBounds.position.y + lBounds.size.y / 2.f});
-        loadingText.setPosition({512.f, 290.f});
+        loadingText.setPosition({512.f, 400.f});
         window.draw(loadingText);
+    } else if (hasError) {
+        rowText.setFillColor(sf::Color(255, 100, 100));
+        rowText.setString("Could not connect to server");
+        auto rBounds = rowText.getLocalBounds();
+        rowText.setOrigin({rBounds.position.x + rBounds.size.x / 2.f, rBounds.position.y + rBounds.size.y / 2.f});
+        rowText.setPosition({512.f, 400.f});
+        window.draw(rowText);
     } else {
-        const float colRankX   = 230.f;
-        const float colNameX   = 290.f;
-        const float colStepsX  = 560.f;
-        const float colTimeX   = 680.f;
-        const float headerY    = 185.f;
+        const float colRankX   = 170.f;
+        const float colNameX   = 240.f;
+        const float colStepsX  = 570.f;
+        const float colTimeX   = 720.f;
+        const float headerY    = 210.f;
 
         headerText.setString("#");
         headerText.setPosition({colRankX, headerY});
@@ -109,26 +110,29 @@ void WinRenderer::render(sf::RenderWindow &window, int steps, float time, bool i
         headerText.setPosition({colTimeX, headerY});
         window.draw(headerText);
 
-        sf::RectangleShape divider({580.f, 1.f});
-        divider.setFillColor(sf::Color(80, 80, 100));
-        divider.setPosition({222.f, 215.f});
+        sf::RectangleShape divider({700.f, 2.f});
+        divider.setFillColor(sf::Color(80, 80, 110));
+        divider.setPosition({162.f, 250.f});
         window.draw(divider);
 
-        float rowY = 225.f;
-        const size_t maxRows = std::min(stats.size(), size_t(5));
+        float rowY = 265.f;
+        const size_t maxRows = std::min(stats.size(), size_t(8));
 
         for (size_t i = 0; i < maxRows; ++i) {
             const auto& entry = stats[i];
 
-            rowText.setFillColor(i == 0 ? sf::Color(255, 215, 0) : sf::Color::White);
+            if (i == 0)      rowText.setFillColor(sf::Color(255, 215, 0));
+            else if (i == 1) rowText.setFillColor(sf::Color(215, 215, 225));
+            else if (i == 2) rowText.setFillColor(sf::Color(205, 127, 50));
+            else             rowText.setFillColor(sf::Color::White);
 
             rowText.setString(std::to_string(i + 1));
             rowText.setPosition({colRankX, rowY});
             window.draw(rowText);
 
             std::string name = entry.username;
-            if (name.length() > 14) {
-                name = name.substr(0, 12) + "..";
+            if (name.length() > 18) {
+                name = name.substr(0, 16) + "..";
             }
             rowText.setString(name);
             rowText.setPosition({colNameX, rowY});
@@ -146,7 +150,7 @@ void WinRenderer::render(sf::RenderWindow &window, int steps, float time, bool i
             rowText.setPosition({colTimeX, rowY});
             window.draw(rowText);
 
-            rowY += 36.f;
+            rowY += 40.f;
         }
 
         if (stats.empty()) {
@@ -154,14 +158,14 @@ void WinRenderer::render(sf::RenderWindow &window, int steps, float time, bool i
             rowText.setString("No records yet for this level!");
             auto rBounds = rowText.getLocalBounds();
             rowText.setOrigin({rBounds.position.x + rBounds.size.x / 2.f, rBounds.position.y + rBounds.size.y / 2.f});
-            rowText.setPosition({512.f, 290.f});
+            rowText.setPosition({512.f, 400.f});
             window.draw(rowText);
         }
     }
 
     auto hBounds = hintText.getLocalBounds();
     hintText.setOrigin({hBounds.position.x + hBounds.size.x / 2.f, hBounds.position.y + hBounds.size.y / 2.f});
-    hintText.setPosition({512.f, 465.f});
+    hintText.setPosition({512.f, 670.f});
     window.draw(hintText);
 
     window.setView(gameView);
